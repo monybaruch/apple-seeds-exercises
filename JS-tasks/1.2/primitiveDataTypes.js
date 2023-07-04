45a5ddccd2683437719d7d95777e65ece8bc41f5
Mony Baruch <monybaruch@gmail.com>
2023-07-04 18:14:49 +0300

    adding js task 1.2

---
+++ JS-tasks/1.2/primitiveDataTypes.js
@@ -0,0 +1,94 @@
+// --Delete the wrong answers--
+// 1. Which of the following is/are strings?
+// a) 4
+// b) 4.0
+// c) '4'
+
+// the answer is c
+
+// 2. Which of the following is/are numbers?
+// a) 4
+// b) 4.0
+// c) '4'
+// d) -4
+
+// a b and c are numbers
+
+// 3. Which of the following is/are booleans?
+// a) true
+// b) false
+// c) "true"
+
+// a and b are booleans
+
+// 4. What is the result of 80 + 71.2?
+// a) 151.2
+// b) 151
+// c) 8071.2
+
+//  a is correct
+
+// 5. What is the result of "80" + 71.2?
+// a) 151.2
+// b) 151
+// c) "8071.2"
+
+// the answer is c
+
+// 6. Does 100 + 30 produce a number or a string?
+// a) number
+// b)string
+
+// 100 +30 produces a number becouse the two of them are numbers
+
+// 7. Does "100" + 30 produce a number or a string?
+// a)number
+// b)string
+
+// "100" + 30 produces a string becouse the first is a string
+
+// create three different ways to declare variables
+
+var myname = "Moshe";
+let myLname = "Sason";
+const myCity = "Beer-Sheva";
+
+// declare a variable and reassign a value to it
+
+let myCar = "volvo";
+
+myCar = "suzuki";
+
+console.log(myCar);
+// create a variable and after that assign a string
+// with its value being: He's got it!
+
+let str = "He's got it!";
+
+// 1. create a variable and assign a value on how
+// much a restaurant bill is.
+
+let bill = 357.58;
+// 2. create a variable and assign a value on how
+// much tax they should pay.
+
+let tax = 0.17;
+// 3. create a variable that will calculate the bill
+// tax and its output would be: Your total bill is
+// <total bill> $.
+
+let taxCalc = 357.58 * 0.17;
+
+let total = bill + taxCalc;
+
+console.log(`Your bill is ${total}`);
+
+// Round the number 50.6 to its nearest integer
+
+let rounded = Math.round(50.6);
+console.log(rounded);
+// Create a variable that is undefined
+
+var a;
+console.log(a);
+var a = 60;
