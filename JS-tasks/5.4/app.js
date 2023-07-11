4d245c4c5e7ad50dd8775dacf783b4b64fcd2524
Mony Baruch <monybaruch@gmail.com>
2023-07-11 13:48:16 +0300

    adding js task 5.4

---
+++ JS-tasks/5.4/app.js
@@ -0,0 +1,28 @@
+/*Create a function that receives one argument, a string color.
+Create a switch statement that:
+1. If the color is yellow, pink, or orange return from the
+function “light color”.
+2. If the color is a blue, purple, or brown return from the
+function “dark color”.
+3. If the color is none of the above return “Unknown color”.
+Note:
+It shouldn’t be case sensitive*/
+
+const colorPicker = (color) => {
+  switch (color) {
+    case "yellow":
+    case "pink":
+    case "orange":
+      return "light color";
+    case "blue":
+    case "purple":
+    case "brown":
+      return "dark color";
+    default:
+      return "Unknown color";
+  }
+};
+
+console.log(colorPicker("brown"));
+console.log(colorPicker("yellow"));
+console.log(colorPicker("red"));
