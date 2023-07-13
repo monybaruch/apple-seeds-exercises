79acda09def9c4158c6c4796b8e069d8c24fc61c
Mony Baruch <monybaruch@gmail.com>
2023-07-13 23:51:41 +0300

    adding 1 more solution to js task 8.4

--- JS-tasks/8.4/app.js
+++ JS-tasks/8.4/app.js
@@ -1,15 +1,15 @@
-// Create a function that takes one argument, an array.
-// Use this array:
+// // Create a function that takes one argument, an array.
+// // Use this array:
 
-// to count over all the letters and return an object with the
-// letter as the key and the letter count as the value.
-// Expected output:
-// { h: 3, e: 4, l: 3, o: 7, g: 3, d: 3, a: 2, y: 2, u: 2, r: 3, w: 1, c:
-// 1, m: 2, t: 1, b: 1, s: 1 }
-// Note:
-// that it shouldn’t be case sensitive
-// Extra:
-// return the letter with the most occurrences as well
+// // to count over all the letters and return an object with the
+// // letter as the key and the letter count as the value.
+// // Expected output:
+// // { h: 3, e: 4, l: 3, o: 7, g: 3, d: 3, a: 2, y: 2, u: 2, r: 3, w: 1, c:
+// // 1, m: 2, t: 1, b: 1, s: 1 }
+// // Note:
+// // that it shouldn’t be case sensitive
+// // Extra:
+// // return the letter with the most occurrences as well
 
 const array = ["Hello", "GoodDay", "YourWelcome", "hotdog", "hamburgers"];
 function counter(array) {
@@ -27,3 +27,23 @@ function counter(array) {
   return countObj;
 }
 console.log(counter(array));
+
+const arr = ["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"];
+
+//another selution with to lower case and with for of loops
+
+const array2 = ["Hello", "Good Day", "YourWelcome", "hotdog", "hamburgers"];
+function lettersCounter(wordsArray) {
+  let counterObj = {};
+  for (const word of wordsArray) {
+    for (const letter of word.toLowerCase()) {
+      if (!Object.keys(counterObj).includes(letter)) {
+        counterObj[letter] = 1;
+      } else if (Object.keys(counterObj).includes(letter)) {
+        counterObj[letter]++;
+      }
+    }
+  }
+  return counterObj;
+}
+console.log(lettersCounter(array2));
