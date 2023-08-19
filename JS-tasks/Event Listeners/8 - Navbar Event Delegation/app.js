f83a2fb37a406f33a203a1333598c47d57978e60
מוני ברוך <monybaruch@gmail.com>
2023-08-19 08:11:57 +0300

    adding solution 8

--- JS-tasks/Event Listeners/8 - Navbar Event Delegation/app.js
+++ JS-tasks/Event Listeners/8 - Navbar Event Delegation/app.js
@@ -6,5 +6,11 @@ console.log(message, ul);
 ul.addEventListener('click', function (event) {
   const clickedItem = event.target;
   const tagName = clickedItem.tagName;
-  console.log(tagName);
+  if (tagName === 'A') {
+    let value = clickedItem.getAttribute('data-link');
+    setTimeout(() => {
+      message.style.display = 'block';
+      message.textContent = `Hi you have clicked ${value}`;
+    }, 3000);
+  }
 });
