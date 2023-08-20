519c90d1f96792540cbf6b18d9fcf29dcabf0055
מוני ברוך <monybaruch@gmail.com>
2023-08-20 21:27:21 +0300

    adding extra hard tasks folder in the event listeners

---
+++ JS-tasks/Event Listeners/extra-hard-tasks/1 - Dynamic Filtering of List Items Exercise/app.js
@@ -0,0 +1,24 @@
+const data = {
+  fruits: ['Apple', 'Banana', 'Cherry'],
+  vegetables: ['Carrot', 'Broccoli', 'Lettuce'],
+  dairy: ['Milk', 'Cheese', 'Yogurt'],
+};
+
+const categories = document.querySelector('.categories');
+const items = document.querySelector('#items');
+
+console.log(categories, items);
+
+categories.addEventListener('click', function (event) {
+  const clickedItem = event.target;
+  let value = clickedItem.getAttribute('data-category');
+  const selectedCategory = data[value];
+  console.log(selectedCategory); // for testing
+  items.innerHTML = '';
+  selectedCategory.forEach((category) => {
+    const newItem = document.createElement('div');
+    newItem.innerHTML = category;
+    newItem.classList.add('item');
+    items.appendChild(newItem);
+  });
+});
