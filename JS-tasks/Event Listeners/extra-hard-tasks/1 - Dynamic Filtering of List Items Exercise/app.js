dbadec18385a03bf01f0dcadfe3125c522da6cdb
מוני ברוך <monybaruch@gmail.com>
2023-08-20 21:28:45 +0300

    adding solution to extra number 1

--- JS-tasks/Event Listeners/extra-hard-tasks/1 - Dynamic Filtering of List Items Exercise/app.js
+++ JS-tasks/Event Listeners/extra-hard-tasks/1 - Dynamic Filtering of List Items Exercise/app.js
@@ -13,7 +13,7 @@ categories.addEventListener('click', function (event) {
   const clickedItem = event.target;
   let value = clickedItem.getAttribute('data-category');
   const selectedCategory = data[value];
-  console.log(selectedCategory); // for testing
+  console.log(selectedCategory); // for testing only
   items.innerHTML = '';
   selectedCategory.forEach((category) => {
     const newItem = document.createElement('div');
