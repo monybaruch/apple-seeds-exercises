757b4eb85bdbac877d931be4a4f1705f6ac42958
מוני ברוך <monybaruch@gmail.com>
2023-09-20 17:03:37 +0300

    adding the logic for the wrongAnswer and the correctAnswer states

--- react-tasks/match-finding/src/components/DigimonInfo.jsx
+++ react-tasks/match-finding/src/components/DigimonInfo.jsx
@@ -1,13 +1,12 @@
-import digimons from '../data/data';
-import { useState } from 'react';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
 import { RxCrossCircled } from 'react-icons/rx';
+import digimons from '../data/data';
+import { useState } from 'react';
 const DigimonInfo = () => {
   const [index, setIndex] = useState(0);
   const [wrongAnswer, setWrongAnswer] = useState(0);
   const [correctAnswer, setCorrectAnswer] = useState(0);
   const { image, digimonName } = digimons[index];
-
   const wrongDigimon = () => {
     setIndex((currentIndex) => {
       const newIndex = (currentIndex - 1 + digimons.length) % digimons.length;
@@ -26,14 +25,27 @@ const DigimonInfo = () => {
     <main>
       <section>
         <div className="text-container">
-          <h4 className="left">Wrong Answers: {wrongAnswer}</h4>
-          <h4 className="right">Right Answers: {correctAnswer}</h4>
+          <h4 className="left">Wrong Answers: {wrongAnswer >= 5 || correctAnswer >= 5 ? 0 : wrongAnswer}</h4>
+          <h4 className="right">Right Answers: {correctAnswer >= 5 || wrongAnswer >= 5 ? 0 : correctAnswer}</h4>
         </div>
-        <img className="image" src={image}></img>
         <div>
-          <header>
-            <h2>{digimonName}</h2>
-          </header>
+          {(() => {
+            if (wrongAnswer >= 5) {
+              return <h1>you lose! go watch digimon and learn the names</h1>;
+            }
+            if (correctAnswer >= 5) {
+              return <h1>you win! you are a digimon expert</h1>;
+            } else {
+              return (
+                <>
+                  <img className="image" src={image}></img>
+                  <header>
+                    <h2>{digimonName}</h2>
+                  </header>
+                </>
+              );
+            }
+          })()}
         </div>
         <button className="btn" onClick={wrongDigimon}>
           <RxCrossCircled />
